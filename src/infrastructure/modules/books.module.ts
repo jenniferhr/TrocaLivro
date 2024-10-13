45dfcdc3770ccc96aeaf0fc656ba771f44153f51
Jennifer Carvalho <jenniferh7637@gmail.com>
2024-10-13 17:39:31 -0300

    add BookEntity to module and config

--- src/infrastructure/modules/books.module.ts
+++ src/infrastructure/modules/books.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from '../../books/application/services/books.service';
 import { BooksController } from '../../books/http-server/controllers/books.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookEntity } from '../persistence/typeorm/entities/book.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BookEntity])],
   controllers: [BooksController],
   providers: [BooksService],
+  exports: [BooksService],
 })
 export class BooksModule {}
