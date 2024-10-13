202957975dfc45131eaf4c469258010fa3552bcb
Jennifer Carvalho <jenniferh7637@gmail.com>
2024-10-13 10:59:35 -0300

    create users CRUD

--- src/users/application/services/users.service.ts
+++ src/users/application/services/users.service.ts
@@ -1,48 +1,86 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UpdateUserDto } from '../../http-server/dto/update-user.dto';
 import { CreateUserCommand } from '../commands/create-user.command';
-import { User } from 'src/users/domain/user';
 import { UserEntity } from 'src/infrastructure/persistence/typeorm/entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserMapper } from 'src/infrastructure/persistence/typeorm/mappers/user.mapper';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    private readonly userMapper: UserMapper,
   ) {}
 
   async create(createUserCommand: CreateUserCommand) {
     const { fullName, email, phoneNumber, address } = createUserCommand;
+
     const existingUser = await this.userRepository.findOne({
       where: { email },
     });
 
     if (existingUser) {
-      throw new Error('User with this email already exists.');
+      throw new ConflictException('User with this email already exists.');
     }
 
-    const user = new User(fullName, email, phoneNumber, address);
+    const user = new UserEntity();
+    user.fullName = fullName;
+    user.email = email;
+    user.phoneNumber = phoneNumber;
+    user.address = address;
+    user.books = [];
 
-    const savedUser = await this.userRepository.save(user);
-
-    return savedUser;
+    try {
+      const savedUser = await this.userRepository.save(user);
+      return savedUser;
+    } catch (err) {
+      throw err;
+    }
   }
 
   async findAll() {
-    return `This action returns all users`;
+    return this.userRepository.find({
+      order: {
+        id: 'ASC',
+      },
+    });
   }
 
   async findOne(id: number) {
-    return `This action returns a #${id} user`;
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found.`);
+    }
+    return user;
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found.`);
+    }
+
+    Object.assign(user, updateUserDto);
+
+    try {
+      return await this.userRepository.save(user);
+    } catch (err) {
+      throw err;
+    }
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} user`;
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found.`);
+    }
+    await this.userRepository.remove(user);
+    return { message: `User with ID ${id} successfully removed.` };
   }
 }
