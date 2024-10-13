4a8e1c16bf860ba1e7e917077b270720d5b716b7
Jennifer Carvalho <jenniferh7637@gmail.com>
2024-10-13 15:29:18 -0300

    add CreateBookDto

--- src/books/http-server/dto/create-book.dto.ts
+++ src/books/http-server/dto/create-book.dto.ts
@@ -1,1 +1,29 @@
-export class CreateBookDto {}
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
+import { BookConditionEnum } from 'src/books/domain/enums/book-condition.enum';
+
+export class CreateBookDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 255)
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 255)
+  author: string;
+
+  @IsNotEmpty()
+  @IsEnum(BookConditionEnum)
+  condition: BookConditionEnum;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 1000)
+  comment?: string;
+}
