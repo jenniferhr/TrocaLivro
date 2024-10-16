f5233fdbff11804e9d1fb95d038387f171f34d8c
Jennifer Carvalho <jenniferh7637@gmail.com>
2024-10-16 20:13:01 -0300

    remove helloworld endpoint from swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
-@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiExcludeEndpoint()
   getHello(): string {
     return this.appService.getHello();
   }
