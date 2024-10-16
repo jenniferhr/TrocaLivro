0504599ac54f5fca76bb9d36131c50bd08cf0e5d
Jennifer Carvalho <jenniferh7637@gmail.com>
2024-10-16 18:30:11 -0300

    add initial swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,17 @@ async function bootstrap() {
       transform: true, // Transforma os campos para o tipo especificado no DTO
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('TrocaLivro API')
+    .setDescription(
+      'API for TrocaLivro, a platform where users can trade books with each other.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
