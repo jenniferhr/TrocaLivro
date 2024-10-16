d8017b0a7aea75a3b785ebaa3615a703b7599cb6
Jennifer Carvalho <jenniferh7637@gmail.com>
2024-10-16 19:31:33 -0300

    adds swagger documentation for user and userbook

--- src/users/http-server/controllers/users.controller.ts
+++ src/users/http-server/controllers/users.controller.ts
@@ -16,7 +16,20 @@ import { CreateUserCommand } from '../../application/commands/create-user.comman
 import { UserBooksService } from 'src/users/application/services/user-books.service';
 import { UpdateUserBookDto } from '../dto/update-user-book.dto';
 import { CreateUserBookDto } from '../dto/create-user-book.dto';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UserEntity } from 'src/infrastructure/persistence/typeorm/entities/user.entity';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(
@@ -25,7 +38,18 @@ export class UsersController {
   ) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
+  @ApiOperation({
+    summary: 'Create a new user',
+    description: 'Add a new user to the system.',
+  })
+  @ApiBody({
+    description: 'Details of the user to be created',
+    type: CreateUserDto,
+  })
+  @ApiCreatedResponse({ description: 'User created successfully.' })
+  @ApiBadRequestResponse({ description: 'Invalid input data.' })
+  @ApiConflictResponse({ description: 'User with this email already exists.' })
+  async create(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
     const createUserCommand = new CreateUserCommand(
       createUserDto.fullName,
       createUserDto.email,
@@ -37,27 +61,70 @@ export class UsersController {
   }
 
   @Get()
-  async findAll() {
+  @ApiOperation({
+    summary: 'Retrieve all users',
+    description: 'Get a list of all users.',
+  })
+  @ApiOkResponse({ description: 'Users retrieved successfully.' })
+  async findAll(): Promise<UserEntity[]> {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  @ApiOperation({
+    summary: 'Get a user by ID',
+    description: 'Retrieve a user by their ID.',
+  })
+  @ApiParam({ name: 'id', description: 'The ID of the user' })
+  @ApiOkResponse({ description: 'User retrieved successfully.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  async findOne(@Param('id') id: string): Promise<UserEntity> {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  @ApiOperation({
+    summary: 'Update user by ID',
+    description: 'Update the information of a specific user.',
+  })
+  @ApiParam({ name: 'id', description: 'The ID of the user' })
+  @ApiOkResponse({ description: 'User updated successfully.' })
+  @ApiBadRequestResponse({ description: 'Invalid input data.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<UserEntity> {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  @ApiOperation({
+    summary: 'Delete a user by ID',
+    description: 'Remove a user from the system by their ID.',
+  })
+  @ApiParam({ name: 'id', description: 'The ID of the user' })
+  @ApiOkResponse({ description: 'User deleted successfully.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  async remove(@Param('id') id: string): Promise<{ message }> {
     return this.usersService.remove(+id);
   }
 
-  // UserBooks;
+  // UserBooks
   @Post(':userId/books')
+  @ApiOperation({
+    summary: 'Add a book to a user',
+    description: "Add a book to the user's collection.",
+  })
+  @ApiParam({ name: 'userId', description: 'The ID of the user' })
+  @ApiBody({
+    description: 'Details of the book to be added',
+    type: CreateUserBookDto,
+  })
+  @ApiCreatedResponse({ description: 'Book added to the user successfully.' })
+  @ApiConflictResponse({ description: 'User already owns this book.' })
+  @ApiNotFoundResponse({ description: 'Book or user not found.' })
+  @ApiBadRequestResponse({ description: 'Invalid input data.' })
   async createUserBook(
     @Param('userId') userId: string,
     @Body() createUserBookDto: CreateUserBookDto,
@@ -69,11 +136,26 @@ export class UsersController {
   }
 
   @Get(':userId/books')
+  @ApiOperation({
+    summary: 'Get all books owned by a user',
+    description: 'Retrieve a list of books owned by a specific user.',
+  })
+  @ApiParam({ name: 'userId', description: 'The ID of the user' })
+  @ApiOkResponse({ description: 'Books retrieved successfully.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
   async findAllUserBooks(@Param('userId') userId: string) {
     return this.userBooksService.findAllByUserId(+userId);
   }
 
   @Get(':userId/books')
+  @ApiOperation({
+    summary: 'Get a specific book owned by a user',
+    description: 'Retrieve details of a specific book owned by the user.',
+  })
+  @ApiParam({ name: 'userId', description: 'The ID of the user' })
+  @ApiParam({ name: 'bookId', description: 'The ID of the book' })
+  @ApiOkResponse({ description: 'Book details retrieved successfully.' })
+  @ApiNotFoundResponse({ description: 'Book or user not found.' })
   async findOneUserBook(
     @Param('userId') userId: string,
     @Query('bookId') bookId: string,
@@ -82,6 +164,14 @@ export class UsersController {
   }
 
   @Get('books/:userBookId')
+  @ApiOperation({
+    summary: "Get details of a user's book by its ID",
+    description:
+      'Retrieve details of a specific book owned by a user using its unique ID.',
+  })
+  @ApiParam({ name: 'userBookId', description: "The ID of the user's book" })
+  @ApiOkResponse({ description: 'Book details retrieved successfully.' })
+  @ApiNotFoundResponse({ description: 'Book not found.' })
   async findByUserBookId(
     @Param('userBookId', new ParseUUIDPipe()) userBookId: string,
   ) {
@@ -89,6 +179,14 @@ export class UsersController {
   }
 
   @Patch('books/:userBookId')
+  @ApiOperation({
+    summary: "Update a user's book details",
+    description: 'Update the details of a specific book owned by the user.',
+  })
+  @ApiParam({ name: 'userBookId', description: "The ID of the user's book" })
+  @ApiOkResponse({ description: "User's book updated successfully." })
+  @ApiBadRequestResponse({ description: 'Invalid input data.' })
+  @ApiNotFoundResponse({ description: 'Book not found.' })
   async updateUserBook(
     @Param('userBookId', new ParseUUIDPipe()) userBookId: string,
     @Body() updateUserBookDto: UpdateUserBookDto,
@@ -97,6 +195,15 @@ export class UsersController {
   }
 
   @Delete('books/:userBookId')
+  @ApiOperation({
+    summary: "Remove a book from a user's collection",
+    description: "Remove a book from a user's possession.",
+  })
+  @ApiParam({ name: 'userBookId', description: "The ID of the user's book" })
+  @ApiOkResponse({
+    description: "Book removed from the user's collection successfully.",
+  })
+  @ApiNotFoundResponse({ description: 'Book not found.' })
   async removeUserBook(
     @Param('userBookId', new ParseUUIDPipe()) userBookId: string,
   ) {
--- src/users/http-server/dto/create-user-book.dto.ts
+++ src/users/http-server/dto/create-user-book.dto.ts
@@ -1,21 +1,35 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
-  IsEnum,
   IsNotEmpty,
   IsNumber,
+  IsEnum,
   IsOptional,
   IsString,
 } from 'class-validator';
 import { BookConditionEnum } from 'src/books/domain/enums/book-condition.enum';
 
 export class CreateUserBookDto {
+  @ApiProperty({
+    description: "ID of the book being added to the user's collection",
+    example: 1,
+  })
   @IsNotEmpty()
   @IsNumber()
   bookId: number;
 
+  @ApiProperty({
+    description: 'Condition of the book',
+    enum: BookConditionEnum,
+    example: BookConditionEnum.NEW,
+  })
   @IsNotEmpty()
   @IsEnum(BookConditionEnum)
   condition: BookConditionEnum;
 
+  @ApiPropertyOptional({
+    description: 'Optional comment about the book condition or other details',
+    example: 'The book is in great condition, only slightly worn on the edges.',
+  })
   @IsOptional()
   @IsString()
   comment?: string;
